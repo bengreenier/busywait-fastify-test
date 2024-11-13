66e2f4d4588d42c9651f4d45cd4063dc48d80bc4
bengreenier <ben@bengreenier.com>
2024-11-13 11:59:24 -0800

    fix(timeouts): Isolate `/slow`

--- index.js
+++ index.js
@@ -10,18 +10,49 @@ server.addHook("onTimeout", async (req, res) => {
 });
 
 server.addHook("onRequestAbort", async (req) => {
-  req.log.error(`aborted`);
+  req.log.error(`aborted ${req.url}`);
 });
+
+/**
+ * A polling function that uses `setTimeout` to avoid blocking the event loop when busy-waiting.
+ */
+function poll(fn, interval) {
+  let timerId;
+
+  function check() {
+    const result = fn();
+    if (result) {
+      clearInterval(timerId);
+    } else {
+      timerId = setTimeout(check, interval);
+    }
+  }
+
+  check();
+}
 
 server.get("/slow", async (req, res) => {
   let waited = 0;
 
-  do {
-    await new Promise((resolve) => setTimeout(resolve, WAIT_INTERVAL_MS));
+  poll(() => {
+    // track that we just waited for X
     waited += WAIT_INTERVAL_MS;
-  } while (waited < WAIT_DURATION_MS);
 
-  res.status(201).send("COMPLETE");
+    // check if we've yet waited for Y
+    // if we have send a response and stop polling
+    if (waited > WAIT_DURATION_MS) {
+      res.status(201).send("COMPLETE");
+      return true;
+    }
+    // otherwise keep pooling
+    else {
+      return false;
+    }
+  }, WAIT_INTERVAL_MS);
+
+  // fastify still requires waiting for the response to be sent
+  // this means we may still have timeouts, but isolated to ONLY this slow route 
+  await res;
 });
 
 server.get("/fast", async (req, res) => {
